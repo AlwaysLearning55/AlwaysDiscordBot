909896855d48ffce3ec2226f5456fc1bac5b2f3f
AlwaysLearning <aleexsander.souza@gmail.com>
2020-10-21 18:58:49 -0300

    Identation error fix.

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ client = commands.Bot(command_prefix = '.')
 
 # Initializing bot's extensions
 for filename in os.listdir('./cogs'):
-if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 # Bot is ready event!
 @client.event
